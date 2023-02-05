f374fda684e9dbd4c3c0cd8c50517cb7190a36aa
Matthew Carney <matthewcarney64@gmail.com>
2023-02-05 22:10:17 +0000

    Added ability to display region

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,14 +1,21 @@
-﻿using System.Collections;
+﻿
+using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using SC4Parser.Files;
-using SC4Parser.Subfiles;
+using SC4Parser.Logging;
+using SC4Parser.Region;
+using SC4Parser;
+
+using Logger = SC4Parser.Logging.Logger;
 
 [RequireComponent(typeof(Terrain))]
 [RequireComponent(typeof(TerrainCollider))]
 public class TerrainGenerator : MonoBehaviour
 {
+
     public Material TerrainMaterial;
 
     private Terrain _terrain = null;
@@ -39,6 +46,9 @@ public class TerrainGenerator : MonoBehaviour
         RegionViewSubfile regionView = null;
         TerrainMapSubfile terrainMap = null;
 
+        Region region = new Region();
+        region.Load(@"C:\Users\Shadowfax\Documents\SimCity 4\Regions\London");
+
         // Get data from save
         using (var save = new SC4SaveFile(@"C:\Users\Shadowfax\Documents\SimCity 4\Regions\London\City - Kensington.sc4"))
         {
@@ -47,10 +57,22 @@ public class TerrainGenerator : MonoBehaviour
         }
 
         // Reformat height map
-        uint sizeX = regionView.CitySizeX;
-        uint sizeY = regionView.CitySizeY;
+        //uint sizeX = regionView.CitySizeX;
+        //uint sizeY = regionView.CitySizeY;
+        //float[,] heightmap = new float[sizeY, sizeX];
+        //float[][] savegameHeightmap = terrainMap.Map;
+        //for (uint x = 0; x < sizeX; x++)
+        //{
+        //    for (uint y = 0; y < sizeY; y++)
+        //    {
+        //        heightmap[y, x] = Map(savegameHeightmap[x][y], 120, 2500, 0, 1);
+        //    }
+        //}
+
+        uint sizeX = (uint) region.Terrain.Length;
+        uint sizeY = (uint) region.Terrain[0].Length;
         float[,] heightmap = new float[sizeY, sizeX];
-        float[][] savegameHeightmap = terrainMap.Map;
+        float[][] savegameHeightmap = region.Terrain;
         for (uint x = 0; x < sizeX; x++)
         {
             for (uint y = 0; y < sizeY; y++)
